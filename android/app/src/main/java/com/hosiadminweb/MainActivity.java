de12c2d19c2e363de87b54b640d39b9177e92774
dai <dai@viempire.com>
2021-10-19 16:42:12 +0700

    read, send, delete message

---
+++ android/app/src/main/java/com/hosiadminweb/MainActivity.java
@@ -0,0 +1,28 @@
+package com.hosiadminweb;
+
+import android.content.Intent;
+import com.facebook.react.ReactActivity;
+import com.react.SmsPackage;
+import com.tkporter.sendsms.SendSMSPackage;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "HosiAdminWeb";
+  }
+
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data)  {
+    super.onActivityResult(requestCode, resultCode, data);
+    //probably some other stuff here
+    SendSMSPackage.getInstance().onActivityResult(requestCode, resultCode, data);
+  }
+
+
+}
